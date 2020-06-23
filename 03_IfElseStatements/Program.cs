39765aafa7e32e242103e2e973f2593b46c00549
traderjeffb <traderjeffb@gmail.com>
2020-06-23 16:40:13 -0400

    Day One afternoon notes

--- 03_IfElseStatements/Program.cs
+++ 03_IfElseStatements/Program.cs
@@ -10,6 +10,72 @@ namespace _03_IfElseStatements
     {
         static void Main(string[] args)
         {
+            bool userIsHungry = false;
+            if (userIsHungry)
+            {
+                Console.WriteLine("Eat Something!");
+            }
+
+            int hourSpentStudying = 1;
+            if(hourSpentStudying < 16)
+            {
+                Console.WriteLine("Are you even trying?");
+            }
+
+            bool choresAreDone = false;
+            if(choresAreDone)
+            {
+                Console.WriteLine("Have fun playing video games");
+            }
+            else
+            {
+                Console.WriteLine("Finish your chores!");
+            }
+
+            Console.WriteLine("How many hours did you sleep?");
+            string input = Console.ReadLine();
+            int totalHours = int.Parse(input);
+            if (totalHours >= 8)
+            {
+                Console.WriteLine("congrats-you are living the dream");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the club");
+                if(totalHours < 3)
+                {
+                    Console.WriteLine("That's rough buddy");
+                }
+            }
+
+            Console.WriteLine("How old are you ?");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+            if (age > 17)
+            {
+                Console.WriteLine("congrats on being an adult);
+            }
+            else
+            {
+                if (age < 6)
+                {
+                    Console.WriteLine("you are a kiddo");
+                }
+                else if (age > 0)
+                {
+                    Console.WriteLine("you are a baby");
+                }
+                else
+                {
+                    Console.WriteLine("You are not even born");
+                } 
+            }
+
+            if (age> 65 || age < 18)
+            {
+                Console.WriteLine("You're still working");
+            }
         }
     }
 }
---
+++ 03_SwitchCases/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SwitchCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("hello");
+                    break;
+                case 2:
+                    Console.WriteLine("What are you doing?");
+                    break;
+                default:
+                    Console.WriteLine("default if no other case is excuted");
+                    break;
+            }
+
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            switch(today)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Hope you are ready to work!");
+                        break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("sorry we are closed");
+                        break;
+
+
+            }
+        }
+
+    }
+}
