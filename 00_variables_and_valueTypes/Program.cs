bae4b94e3a41e9e5bf8fba01045657fc818aff08
traderjeffb <traderjeffb@gmail.com>
2020-06-23 14:57:34 -0400

    first commit

---
+++ 00_variables_and_valueTypes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_variables_and_valueTypes
+{ //use enums if you have static values. something you don't anticipate changing. note-Pascal casing. 
+    enum Pastrytype { Cake, Cupcake, Croissant, Donut, Cookie, Scone, Danish }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclaredAndInitialized = false;
+            isDeclaredAndInitialized = true;
+
+            char character = 'a'; //use single quotes for a character. Double quotes are for strings. 
+            char symbol = '&';
+            char number = '5';
+            char space = ' '; //can be a symbol, number or even a space 
+            char specialCharacter = '/n';//or even a special character
+
+            //Whole Numbers
+            byte byteExample = 255; //max numbe is 256
+            sbyte sByteExample = -128; //
+            short shortExample = 32767;
+            Int16 anotherShortExample = 32000;
+            int intMin = -2147483648;
+            Int32 intMax = 2147483648;
+            long longExample = 9223372036854775807;
+            Int64 longMin = 9223372036854775807;//same as above 
+            
+            //below was used for breakpoint practice
+            int a = 7;
+            int b = -7000;
+
+            byte age = 164;
+            //--------------------------------
+
+            //Decimals
+            float floatExample = 1045231f;//f makes this a float
+            double doubleExample = 178905278907036d;
+
+            //if you look at the print out of the below numbers it show the cutoff points of the different types
+            Console.WriteLine(1.8181581691651616416f);
+            Console.WriteLine(1.8181581691651616416d);
+            Console.WriteLine(1.8181581691651616416m);
+            Console.ReadLine();
+
+            //Enums DECLAIRED outside the class-see top of this page.use enums if you have static values- something you don't anticipate changing. note-Pascal casing. 
+            Pastrytype myPastry = Pastrytype.Croissant;
+            Pastrytype anotherOne = Pastrytype.Donut;
+
+            //Structs -have default values
+            Int32 = 42;
+            DateTime today = DateTime.Today;
+            DateTime birthday = new DateTime(1805, 11, 24);
+            DateTime test = new DateTime();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 01_ReferenceTypes/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strings
+            string declared;
+            declared = "This is insitialized";
+
+            string declarationAndInitialization = "This is both declaring and initialzing.";
+
+            Console.WriteLine("What is your first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            //Concatenation
+            string concatenatiedFullName = firstName + " " + lastName;
+            //Composite
+            string compositeFullName = string.Format("{0} {1}", firstName, lastName);
+            //interpolation
+            string InterpolatedFullName = $"{firstName} {lastName}";
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(concatenatiedFullName);
+            Console.WriteLine(compositeFullName);
+            Console.WriteLine(InterpolatedFullName);
+            Console.ReadLine();
+
+            //--Collections
+            //Array --once you make an array it has a specific amount of space you cannot change it.
+            string stringExample = "Hello World!";
+            string[] stringArray = { "Hello", "World", "Why", "is it", "always", stringExample, "?" };
+            string thirdItem = stringArray[2];
+            Console.WriteLine(thirdItem);
+            stringArray[0] = "Hey there";
+            Console.WriteLine(stringArray[0]);
+
+            //--Lists
+            List<string> listOfStrings = new List<string>();
+            List<int> listOfIntegers = new List<int>();
+            listOfStrings.Add("First string of our lists");
+            listOfIntegers.Add(4242);
+            listOfIntegers.Add(12345);
+            Console.WriteLine(listOfIntegers[0]);
+
+            //--Queue
+            Queue<string> firstInFirstOut = new Queue<string>();
+            firstInFirstOut.Enqueue("I'm First");
+            firstInFirstOut.Enqueue("I'm next");
+            string firstItem = firstInFirstOut.Dequeue();
+            Console.WriteLine(firstItem);
+
+            //--Dictionaries has a key and value pair
+            Dictionary<int, string> keyAndValue = new Dictionary<int, string>();
+            keyAndValue.Add(7, "Agent");
+            string valueSeven = keyAndValue[7];
+            Console.WriteLine(valueSeven);
+
+            //--other collection Examples
+            SortedList<int, string> SortedKeyAndValue = new SortedList<int, string>();
+            HashSet<int> uniqueList = new HashSet<int>();
+            Stack<string> lastInFirstOut = new Stack<string>();
+
+            //Classes must be instanciated with "new" keyword
+            Random rng = new Random();
+            int randomNumber = rng.Next();
+            Console.WriteLine(randomNumber);
+
+
+            Console.ReadKey();
+        }
+    }
+}
